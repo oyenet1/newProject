86978b5917730139a0360e42c585b7413634f33a
oyenet1 <bowofadeoyerinde@gmail.com>
2022-03-11 11:45:19 +0100

    new laravel 9 projects

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    mode: 'jit',
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                'rubik': ["'Rubik', sans-serif"],
+              },
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'primary': '#6633ff',
+                'purple': '#3f3cbb',
+                'header': '#0A376E',
+                'background': '#f6F3ff',
+              },
+        },
+    },
+    variants: {
+      extend: {},
+    },
+
+    plugins: [],
+};
